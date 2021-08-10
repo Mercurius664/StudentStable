e2685daf1f0f09b2291e634b6d7357398c8be41e
Mercurius664 <83498582+Mercurius664@users.noreply.github.com>
2021-08-10 20:07:34 +0300

    rename methods in DOMxmlWriter class , minimized count of parameters in function appendStudent

--- src/com/IvashkinIgor/dialog/delete/DeleteByCountOfBrothersAndSistersController.java
+++ src/com/IvashkinIgor/dialog/delete/DeleteByCountOfBrothersAndSistersController.java
@@ -4,7 +4,6 @@ import com.IvashkinIgor.model.Model;
 import com.IvashkinIgor.model.Student;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
